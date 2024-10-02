80fcdd4c20a0cea7fe97db7c95d2b5ace0fd0f6a
Sujal Chauhan <chauhansujal1107@gmail.com>
2024-10-03 01:21:08 +0530

    added google console crawlers robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const sitemapConfig = {
+    siteUrl: 'https://sujalchauhan.in', // Replace with your actual domain
+    generateRobotsTxt: true,            // (optional) Generates robots.txt file
+    exclude: ['/private-page'],         // Exclude specific pages (if any)
+}
+
+module.exports = sitemapConfig
